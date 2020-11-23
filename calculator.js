4f913b4a17ad0b81cfd42df18ebd431c08c9fffb
Thoosk <thomas.skant@gmx.at>
2020-11-23 19:10:34 +0100

    updated whle code base -add eventHandler for numbers, operations, equal and clear buttons -update the display of the calculator after every input -the user can input different operations after another (e.g. 7 + 5 - 2 + 3 = ) and the calculator will take the result of the previous calculation and work with that (WIP) -add warnings

--- calculator.js
+++ calculator.js
@@ -20,31 +20,119 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   if (num1 === 0) {
-    return "Close one, you nearly ended us all by trying to divide zero";
+    alert("Close one, you nearly ended us all by trying to divide zero");
+    return 0;
   }
 
   return num1 / num2;
 }
 
-function validate(x) {
-  if (isNaN(x)) {
-    return NaN;
-  }
-}
-
 function operate(input1, input2, operation) {
-  if (validate(input1) === NaN || validate(input2) === NaN) {
+  if (isNaN(input1) || isNaN(input2)) {
     return "Only numbers are allowed";
   }
 
   switch (operation) {
-    case "add":
+    case "+":
       return add(input1, input2);
-    case "subtract":
+    case "-":
       return subtract(input1, input2);
-    case "multiply":
+    case "×":
       return multiply(input1, input2);
-    case "divide":
+    case "÷":
       return divide(input1, input2);
   }
 }
+
+let displayInput = document.querySelector("#calc-field h1");
+let calculationText = document.querySelector("#calc-field p");
+//functionality for numbers
+let numberArray = Array.from(document.querySelectorAll(".number"));
+let inputArray = [];
+
+numberArray.forEach((num) => {
+  num.addEventListener("click", () => {
+    if (displayInput.offsetWidth >= 350) {
+      alert("You reached the maximum of numbers that can be entered");
+      return;
+    }
+    if (parseInt(displayInput.innerText) === result) {
+      displayInput.innerText = "";
+    }
+    displayInput.innerText += num.getAttribute("value");
+    inputArray.push(num.getAttribute("value"));
+  });
+});
+
+//functionality for operators
+let operatorArray = Array.from(document.querySelectorAll(".operator"));
+let firstInput = 0;
+let secondInput = 0;
+let intermediateResult = 0;
+let result = 0;
+let currOperation = "";
+
+operatorArray.forEach((oper) => {
+  oper.addEventListener("click", () => {
+    if (inputArray.length === 0) {
+      return;
+    }
+
+    if (result !== 0) {
+      firstInput = result;
+    }
+
+    if (firstInput !== 0) {
+      console.log("Wat");
+
+      intermediateResult = operate(
+        firstInput,
+        parseInt(inputArray.reduce((acc, curr) => acc + curr)),
+        currOperation
+      );
+      console.log(intermediateResult);
+      calculationText.innerText = `${intermediateResult} ${oper.getAttribute(
+        "value"
+      )}`;
+      firstInput = intermediateResult;
+      currOperation = oper.getAttribute("value");
+    } else {
+      firstInput = parseInt(inputArray.reduce((acc, curr) => acc + curr));
+      calculationText.innerText = `${
+        displayInput.innerText
+      } ${oper.getAttribute("value")}`;
+      currOperation = oper.getAttribute("value");
+    }
+
+    // currOperation = oper.getAttribute("value");
+
+    displayInput.innerText = "";
+    inputArray = [];
+  });
+});
+
+//functionality for result
+let equalSign = document.querySelector(".equal");
+equalSign.addEventListener("click", () => {
+  if (inputArray.length === 0) {
+    return;
+  }
+
+  secondInput = parseInt(inputArray.reduce((acc, curr) => acc + curr));
+  result = operate(firstInput, secondInput, currOperation);
+  calculationText.innerText = `${firstInput} ${currOperation} ${secondInput}`;
+  displayInput.innerText = result;
+});
+
+//functionality for Clear
+let clear = document.querySelector(".clearing");
+clear.addEventListener("click", () => {
+  firstInput = 0;
+  secondInput = 0;
+  result = 0;
+  intermediateResult = 0;
+  currOperation = "";
+  inputArray = [];
+  calculationText.innerText = "x";
+  displayInput.innerText = "";
+});
